aac2c9edf156c08f8b46eeae37c8ca33d654a9eb
areller <lekararik@gmail.com>
2019-11-19 13:50:51 -0500

    formatting

--- src/RediSharp/Lib/Json.cs
+++ src/RediSharp/Lib/Json.cs
@@ -2,21 +2,21 @@
 using RediSharp.Enums;
  using RediSharp.RedIL.Resolving.Attributes;
  using RediSharp.RedIL.Resolving.CommonResolvers;
- 
- namespace RediSharp.Lib
- {
-     public static class Json
-     {
-         [RedILResolve(typeof(CallLuaBuiltinStaticMethodResolver), LuaBuiltinMethod.JsonEncode)]
-         public static string Encode(object obj)
-         {
-             return JsonConvert.SerializeObject(obj);
-         }
- 
-         [RedILResolve(typeof(CallLuaBuiltinStaticMethodResolver), LuaBuiltinMethod.JsonDecode)]
-         public static T Decode<T>(string json)
-         {
-             return JsonConvert.DeserializeObject<T>(json);
-         }
-     }
- }
+
+namespace RediSharp.Lib
+{
+    public static class Json
+    {
+        [RedILResolve(typeof(CallLuaBuiltinStaticMethodResolver), LuaBuiltinMethod.JsonEncode)]
+        public static string Encode(object obj)
+        {
+            return JsonConvert.SerializeObject(obj);
+        }
+
+        [RedILResolve(typeof(CallLuaBuiltinStaticMethodResolver), LuaBuiltinMethod.JsonDecode)]
+        public static T Decode<T>(string json)
+        {
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+    }
+}
