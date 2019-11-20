591eef64df24b28b19617ca28e808c5118e2dd31
areller <areller.gm@gmail.com>
2019-11-19 19:05:36 -0500

    Update ClientExtensions.cs

--- tests/RediSharp.IntegrationTests/Extensions/ClientExtensions.cs
+++ tests/RediSharp.IntegrationTests/Extensions/ClientExtensions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using StackExchange.Redis;
 
@@ -6,13 +7,27 @@ namespace RediSharp.IntegrationTests.Extensions
 {
     public static class ClientExtensions
     {
+        private static object _globalSync = new object();
+        
         public static async Task<TRes> ExecuteP<TRes>(this Client<IDatabase> client, Function<IDatabase, TRes> action,
             RedisValue[] arguments = null, RedisKey[] keys = null)
         {
             var handle = client.GetHandle(action);
             await handle.Init();
-            
-            Console.WriteLine(handle.Artifact);
+
+            using (var writer = new System.IO.StreamWriter(System.Console.OpenStandardOutput()))
+            {
+                lock (_globalSync)
+                {
+                    Console.WriteLine("===========================");
+                    Console.WriteLine(handle.Artifact);
+                    Console.WriteLine("===========================");
+                    
+                    writer.WriteLine("===========================");
+                    writer.WriteLine(handle.Artifact);
+                    writer.WriteLine("===========================");
+                }
+            }
 
             var res = await handle.Execute(arguments, keys);
             return res;
